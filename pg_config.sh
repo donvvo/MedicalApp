9643c5fef9bdad652b942491dbd7d47373704fd8
Andrew Jung <andrewjjung47@gmail.com>
2015-10-24 23:10:12 -0400

    app setup and started migrating templates and assets

--- pg_config.sh
+++ pg_config.sh
@@ -3,3 +3,11 @@ apt-get -qqy install python-dev
 apt-get -qqy install libpq-dev
 apt-get -qqy install postgresql python-psycopg2
 apt-get -qqy install python-pip
+apt-get -qqy install libjpeg-dev
+
+sudo pip install -r /vagrant/MedicalApp/requirements/local.txt
+
+su postgres -c 'createuser -dRS vagrant'
+su vagrant -c 'createdb'
+su vagrant -c 'createdb medicalapp'
+sudo -u postgres psql -U postgres -d postgres -c "alter user vagrant with password 'medicalapp'"
