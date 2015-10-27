4a04f659ba4aff11692cb6161d5f8f26d17a5e59
Andrew Jung <andrewjjung47@gmail.com>
2015-10-27 09:42:55 -0400

    Split model.py into smaller files

--- MedicalApp/MedicalForms/models.py
+++ MedicalApp/MedicalForms/models.py
@@ -3,68 +3,7 @@ from django.utils.encoding import python_2_unicode_compatible
 
 from MedicalApp.users.models import User
 
-
-# Patient information form choices
-GENDER_CHOICES = (
-    ('Male', 'Male'),
-    ('Female', 'Female')
-)
-OCCUPATIONAL_STATUS_CHOICES = (
-    ('Full-time', 'Full-time'),
-    ('Part-time', 'Part-time'),
-    ('Modified Duties', 'Modified Duties'),
-    ('NotReturned', 'NotReturned'),
-    ('Unemployed', 'Unemployed'),
-    ('Retired', 'Retired')
-)
-JOB_REQUIREMENTS_CHOICES = (
-    ('Mainly sitting', 'Mainly sitting'),
-    ('Mainly standing', 'Mainly standing'),
-    ('Light labour', 'Light labour'),
-    ('Heavy labour', 'Heavy labour')
-)
-MARITAL_STATUS_CHOICES = (
-    ('Married', 'Married'),
-    ('Single', 'Single'),
-    ('Divorced', 'Divorced'),
-    ('Common Law', 'Common Law')
-)
-
-# Patient information form
-@python_2_unicode_compatible
-class General(models.Model):
-    user = models.OneToOneField(User)
-    today_date = models.DateTimeField(auto_now_add=True, blank=True)
-    accident_date = models.DateTimeField(blank=True)
-    date_of_birth = models.DateTimeField(blank=True)
-    name = models.CharField(max_length=100, blank=True)
-    gender = models.CharField(max_length=10, blank=True)
-    address = models.CharField(max_length=200, blank=True)
-    city = models.CharField(max_length=100, blank=True)
-    postal_code = models.CharField(max_length=10, blank=True)
-    email = models.EmailField(blank=True)
-    first_language = models.CharField(max_length=100, blank=True)
-    home_phone = models.CharField(max_length=20, blank=True)
-    mobile_phone = models.CharField(max_length=20, blank=True)
-    work_phone = models.CharField(max_length=20, blank=True)
-    occupation = models.CharField(max_length=100, blank=True)
-    modified_duties_availability = models.NullBooleanField()
-    occupational_status = models.CharField(max_length=20,
-                                           choices=OCCUPATIONAL_STATUS_CHOICES,
-                                           blank=True)
-    job_requirements = models.CharField(max_length=20,
-                                        choices=JOB_REQUIREMENTS_CHOICES,
-                                        blank=True)
-    marital_status = models.CharField(max_length=20,
-                                      choices=MARITAL_STATUS_CHOICES,
-                                      blank=True)
-    number_of_children = models.IntegerField(blank=True)
-    ages = models.CharField(max_length=100, blank=True)
-    emergency_contact_name = models.CharField(max_length=100, blank=True)
-    emergency_contact_phone = models.CharField(max_length=20, blank=True)
-    emergency_contact_relationship = models.CharField(max_length=20, blank=True)
-
-
+from .patient_info_models import *
 
 
 # Health history questions.
@@ -193,15 +132,22 @@ DOMINANT_HAND_CHOICES = (
 class PassengerLocation(models.Model):
     choice = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.choice
+
 
 @python_2_unicode_compatible
 class BodyPart(models.Model):
     choice = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.choice
+
 
 # Accident history
 @python_2_unicode_compatible
 class AccidentHistory(models.Model):
+    user = models.OneToOneField(User)
     description_and_location = models.TextField(blank=True)
     road_condition = models.CharField(max_length=10,
                                       choices=ROAD_CONDITION_CHOICES,
@@ -218,7 +164,7 @@ class AccidentHistory(models.Model):
     vehicle_towed = models.NullBooleanField()
     impact_with_objects = models.CharField(max_length=50, blank=True)
     exit_from_vehicle = models.CharField(max_length=30,
-                                         chocies=EXIT_FROM_VEHICLE_CHOICES,
+                                         choices=EXIT_FROM_VEHICLE_CHOICES,
                                          blank=True)
     loss_of_consciousness = models.NullBooleanField()
     safety_equipment = models.NullBooleanField()
@@ -229,3 +175,7 @@ class AccidentHistory(models.Model):
                                      choices=DOMINANT_HAND_CHOICES,
                                      blank=True)
     body_part_collision = models.ManyToManyField(BodyPart, blank=True)
+
+    def __str__(self):
+        return 'Health history for ' + self.user.first_name
+        + ' ' + self.user.last_name
---
+++ MedicalApp/MedicalForms/patient_info_models.py
@@ -0,0 +1,69 @@
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+from MedicalApp.users.models import User
+
+# Patient information form choices
+GENDER_CHOICES = (
+    ('Male', 'Male'),
+    ('Female', 'Female')
+)
+OCCUPATIONAL_STATUS_CHOICES = (
+    ('Full-time', 'Full-time'),
+    ('Part-time', 'Part-time'),
+    ('Modified Duties', 'Modified Duties'),
+    ('NotReturned', 'NotReturned'),
+    ('Unemployed', 'Unemployed'),
+    ('Retired', 'Retired')
+)
+JOB_REQUIREMENTS_CHOICES = (
+    ('Mainly sitting', 'Mainly sitting'),
+    ('Mainly standing', 'Mainly standing'),
+    ('Light labour', 'Light labour'),
+    ('Heavy labour', 'Heavy labour')
+)
+MARITAL_STATUS_CHOICES = (
+    ('Married', 'Married'),
+    ('Single', 'Single'),
+    ('Divorced', 'Divorced'),
+    ('Common Law', 'Common Law')
+)
+
+
+# Patient information form
+@python_2_unicode_compatible
+class PatientInformation(models.Model):
+    user = models.OneToOneField(User)
+    today_date = models.DateTimeField(auto_now_add=True, blank=True)
+    accident_date = models.DateTimeField(blank=True)
+    date_of_birth = models.DateTimeField(blank=True)
+    name = models.CharField(max_length=100, blank=True)
+    gender = models.CharField(max_length=10, blank=True)
+    address = models.CharField(max_length=200, blank=True)
+    city = models.CharField(max_length=100, blank=True)
+    postal_code = models.CharField(max_length=10, blank=True)
+    email = models.EmailField(blank=True)
+    first_language = models.CharField(max_length=100, blank=True)
+    home_phone = models.CharField(max_length=20, blank=True)
+    mobile_phone = models.CharField(max_length=20, blank=True)
+    work_phone = models.CharField(max_length=20, blank=True)
+    occupation = models.CharField(max_length=100, blank=True)
+    modified_duties_availability = models.NullBooleanField()
+    occupational_status = models.CharField(max_length=20,
+                                           choices=OCCUPATIONAL_STATUS_CHOICES,
+                                           blank=True)
+    job_requirements = models.CharField(max_length=20,
+                                        choices=JOB_REQUIREMENTS_CHOICES,
+                                        blank=True)
+    marital_status = models.CharField(max_length=20,
+                                      choices=MARITAL_STATUS_CHOICES,
+                                      blank=True)
+    number_of_children = models.IntegerField(blank=True)
+    ages = models.CharField(max_length=100, blank=True)
+    emergency_contact_name = models.CharField(max_length=100, blank=True)
+    emergency_contact_phone = models.CharField(max_length=20, blank=True)
+    emergency_contact_relationship = models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+        return 'Patient information for ' + self.user.first_name
+        + ' ' + self.user.last_name
