344e5da1afb23e9497aca5f33e559f5e81393ef7
Andrew Jung <andrewjjung47@gmail.com>
2015-11-07 17:06:36 -0500

    Polish patient appointment page.

--- MedicalApp/MedicalAppointments/urls.py
+++ MedicalApp/MedicalAppointments/urls.py
@@ -4,9 +4,11 @@ from __future__ import absolute_import, unicode_literals
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
+from .views import AppointmentView
+
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='medicalappointments/appointment.html'), name="appointments"),
+    url(r'^$', AppointmentView.as_view(), name="appointments"),
     url(r'^new/$', TemplateView.as_view(template_name='medicalappointments/new_appointment.html'), name="new_appointments"),
     url(r'^timetable/$', TemplateView.as_view(template_name='medicalappointments/timetable_doctor.html'), name="timetable_doctor"),
     url(r'^new/timetable$', TemplateView.as_view(template_name='medicalappointments/timetable_patient.html'), name="timetable_patient"),
--- MedicalApp/MedicalAppointments/views.py
+++ MedicalApp/MedicalAppointments/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import DetailView, ListView
+
+from braces.views import LoginRequiredMixin
+
+from .models import Booking
 
 # Create your views here.
+class AppointmentView(LoginRequiredMixin, ListView):
+    model = Booking
+    template_name = "medicalappointments/appointment.html"
+
+    def get_queryset(self):
+        bookings = self.model.objects.filter(patient=self.request.user.patient).all()
+        print bookings
+        return bookings
