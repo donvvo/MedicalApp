ea5a4c5ad55376b64aa1a5edd986d2a0428a805c
Andrew Jung <andrewjjung47@gmail.com>
2015-11-07 01:09:12 -0500

    Added doctor and patient models.

--- MedicalApp/MedicalAppointments/models.py
+++ MedicalApp/MedicalAppointments/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+from MedicalApp.users.models import User
+
+
 # Create your models here.
+class Clinic(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=20)
+    address = models.CharField(max_length=200)
+    postal_code = models.CharField(max_length=10)
+
+
+class Doctor(models.Model):
+    user = models.OneToOneField(User, primary_key=True)
+
+
+class Patient(models.Model):
+    user = models.OneToOneField(User, primary_key=True)
