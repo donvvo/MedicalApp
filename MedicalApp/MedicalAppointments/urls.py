7a83150baafb761e729337e38f0b980abab1d276
Andrew Jung <andrewjjung47@gmail.com>
2015-11-12 02:02:30 -0500

    Fixed all the bugs in appointment.

--- MedicalApp/MedicalAppointments/urls.py
+++ MedicalApp/MedicalAppointments/urls.py
@@ -3,14 +3,13 @@ from __future__ import absolute_import, unicode_literals
 
 from django.conf.urls import url
 
-from .views import AppointmentView, NewAppointmentView, get_clinics, PatientTimetableView, save_booking,\
+from .views import AppointmentView, NewAppointmentView, get_clinics, PatientTimetableView, \
     DoctorTimetableView, ClinicProfileView, ClinicListView
 
 
 urlpatterns = [
     url(r'^$', AppointmentView.as_view(), name="appointments"),
     url(r'^new/$', NewAppointmentView.as_view(), name="new_appointments"),
-    url(r'^new/save/$', save_booking, name="save_booking"),
     url(r'^timetable/$', DoctorTimetableView.as_view(), name="timetable_doctor"),
     url(r'^new/timetable/(?P<clinic>[\w+]+)/(?P<specialty>\w+)/$', PatientTimetableView.as_view(), name="timetable_patient"),
     url(r'^clinics/.json$', get_clinics, name="get_clinics"),
