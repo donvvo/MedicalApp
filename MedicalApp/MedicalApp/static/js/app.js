b8938099e09feee03e2971455cdc3a0f9c2b97d9
Andrew Jung <andrewjjung47@gmail.com>
2015-11-23 13:55:01 -0500

    Added collision animation.

--- MedicalApp/MedicalApp/static/js/app.js
+++ MedicalApp/MedicalApp/static/js/app.js
@@ -1,9 +1,33 @@
 $(function() {
     var $successBtn = $('#success-btn');
-    console.log($successBtn);
     var $doctorSignupSuccess = $('#doctor-signup-success');
-    console.log($doctorSignupSuccess);
     $successBtn.click(function() {
         $doctorSignupSuccess.remove();
     });
+
+    // Car crash animations
+    $frontEndCollision = $('#front-end-collision');
+    function frontEndLoop() {
+        $frontEndCollision.delay(1000).css("left", "50px");
+        $frontEndCollision.animate({"left": "-=50"}, 1000, frontEndLoop);
+    }
+    frontEndLoop();
+    $sideEndCollision = $('#side-end-collision');
+    function sideEndLoop() {
+        $sideEndCollision.delay(1000).css("left", "0");
+        $sideEndCollision.animate({"left": "+=50"}, 1000, sideEndLoop);
+    }
+    sideEndLoop();
+    $sideSwipeCollision = $('#side-swipe-collision');
+    function sideSwipeLoop() {
+        $sideSwipeCollision.delay(1000).css("top", "30px");
+        $sideSwipeCollision.animate({"top": "-=30"}, 1000, sideSwipeLoop);
+    }
+    sideSwipeLoop();
+    $rearEndCollision = $('#rear-end-collision');
+    function rearEndLoop() {
+        $rearEndCollision.delay(1000).css("left", "0");
+        $rearEndCollision.animate({"left": "+=50"}, 1000, rearEndLoop);
+    }
+    rearEndLoop();
 });
